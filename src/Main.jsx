5bd724af965cfe67334c7c30e0e3d34ff7e05b19
Maeva <maeva-froger@outlook.fr>
2023-10-27 15:57:32 +0200

    Updates

---
+++ src/Main.jsx
@@ -0,0 +1,20 @@
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Projects from "./components/projects/projects";
+import Skills from "./components/skills/skills";
+
+
+export default function Main(){
+    return (
+        <div className="portfolio">
+            <Navigation />
+            <main>
+                <About />
+                <Projects />
+                <Skills />
+                <Contact />
+            </main>
+        </div>
+    );
+}
---
+++ src/components/skills/skills.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Skills() {
+return (
+    <section id="skills">
+        <h2>Skills</h2>
+    </section>
+);
+}
