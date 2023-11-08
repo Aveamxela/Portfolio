d999e14a050846778cefdfa82357242608150977
Maeva <maeva-froger@outlook.fr>
2023-11-08 15:05:21 +0100

    animation loader

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,6 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Loader from "./components/loader/Loader";
 import Navigation from "./components/navigation/Navigation";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
@@ -7,6 +8,7 @@ import Skills from "./components/skills/skills";
 export default function Main(){
     return (
         <div className="portfolio">
+            <Loader/>
             <Navigation />
             <main>
                 <About />
